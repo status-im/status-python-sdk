7c67f58b5efb009b820b5289e4f5f1c9a1ee02be
Nick Ninov <n.ninov99@gmail.com>
2026-09-22 22:13:41 +0300

    test: Add new CHANGELOG
    - Validate if the new branch version has updated CHANGELOG.md

--- .github/tests/version.py
+++ .github/tests/version.py
@@ -1,7 +1,7 @@
 """
 Verify that pyproject.toml version is greater than `main` branch want
 """
-import tomllib
+import tomllib, re
 import urllib.request
 from packaging.version import Version
 from pathlib import Path
@@ -30,11 +30,26 @@ def fetch_base_version(repo: str = "status-im/status-python-sdk", branch: str = 
     with urllib.request.urlopen(url, timeout=30) as response:
         return read_version(response.read())
 
+def read_changelog() -> str:
+    """
+    Read `CHANGELOG.md` file from the working tree.
+    """
+    path = Path(__file__).resolve().parents[2] / "CHANGELOG.md"
 
+    with open(path, "r") as f:
+        changelog = f.read()
+
+    return changelog
 
 if __name__ == "__main__":
+    # (1) Check if new version is greater than current version
     current_version = Version(fetch_current_version())
     uploaded_version = Version(fetch_base_version())
-
     if current_version <= uploaded_version:
         raise Exception(f"Branch version ({current_version}) must be greater than GitHub `master` ({uploaded_version}). Please update pyproject.toml")
+
+    # (2) Check if new version is in CHANGELOG.md
+    pattern = r"^##\s*\[" + str(current_version).replace(".", "\\.") + r"\]\s*-\s*\d{4}-\d{2}-\d{2}\s*$"
+    exists = bool(re.search(pattern, read_changelog(), re.M))
+    if not exists:
+        raise Exception(f"CHANGELOG for {current_version} does not exist. Please update CHANGELOG.md")
